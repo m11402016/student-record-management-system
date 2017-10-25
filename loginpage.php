32060b257680fa6756fcdab6300dfb1bb5cd44e5
m11402016 <32332939+m11402016@users.noreply.github.com>
2017-10-25 18:45:02 +0000

    Add files via upload

---
+++ loginpage.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<body bgcolor="powderblue">
+<center>
+<div style="background-color:green;color:red;padding:10px">
+<img src="logo.png" alt="logo" width="125" height="142"> 
+</div>
+<br>
+<div style="background-color:yellow;color:black;padding:20px">
+<h1> STUDENT RECORD CELL </h1>
+<form action = "home_page.php" method = "post">
+<input type ="text" name="naam">
+<input type ="password" password="passkey">
+<br><br>
+<input type ="submit" name="submit">
+ <a href="forget_password.php">Forget Password?</a> 
+</form>
+</div>
+</center>
+<h5>My <span style="color:red">Important</span> policies</h5>
+
+</body>
+</html>
